6ac7d2ea9414f40e3497724cbfe8daa449b02870
galmayda <galmayda@gmail.com>
2019-10-31 03:03:30 +0700

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -12,7 +12,9 @@
 */
 
 Route::get('/', 'PagesController@home');
-Route::get('/order', 'PagesController@order');
+Route::get('/order', 'OrderController@order');
+Route::post('/order', 'OrderController@postOrder');
 Route::get('/login', 'AuthController@login');
 Route::post('/register', 'AuthController@postRegister')->name('register');
 Route::post('/login', 'AuthController@postLogin')->name('login');
+Route::get('/logout', 'AuthController@logout');
