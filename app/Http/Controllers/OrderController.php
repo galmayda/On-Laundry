83d22e6c55936402941220001cdc246289aadda3
galmayda <galmayda@gmail.com>
2019-10-31 03:01:18 +0700

    Create OrderController.php

---
+++ app/Http/Controllers/OrderController.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\OrderModel;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Session;
+
+class OrderController extends Controller
+{
+    //    
+    public function order(){
+        if(Session::has('name')){
+            return view('order');
+        }
+        else{
+            return view('login');
+        }
+    }
+
+    public function postOrder(Request $request){
+        
+    }
+}
