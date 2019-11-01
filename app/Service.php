0755df01053e4ae994ed95b9503835f1cdb94e82
galmayda <galmayda@gmail.com>
2019-11-02 01:00:39 +0700

    Rename model

---
+++ app/Service.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class ServicesModel extends Model
+{
+    //
+    protected $guarded = [];
+
+    protected $table = 'services';
+}
